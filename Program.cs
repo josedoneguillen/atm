d7a733c9a06ed42610cff1561ab9f63a9c64e6ac
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-03 01:38:00 -0400

    Inicializando proyecto en VS 2019

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
