6b9f16ba467c12325086d6adcbed08dd062796a2
Angel Lopez <enmanuelm15@hotmail.com>
2020-03-22 11:39:29 -0400

    first commit

---
+++ ITLA ATM/C_usuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITLA_ATM
+{
+    class C_usuarios
+    {
+        string numero_tarjeta { get; set; }
+        public string nombre { get; set; } 
+            public string apellido{get; set;} 
+            public string contra{get; set;}
+        public double saldo { get; set; }
+        public bool isadmin { get; set; }
+        public void C_usuario(string tarjeta, string nomb, string apell, string cont,double sal, bool admin=false )
+        {
+            numero_tarjeta = tarjeta;
+            nombre = nomb;
+            apellido = apell;
+            contra = cont;
+            saldo = sal;
+            isadmin = admin; 
+        }
+    }
+}
---
+++ ITLA ATM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITLA_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
